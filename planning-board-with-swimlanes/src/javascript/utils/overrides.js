6966112db600e5439ee641699c156e6bfc924ff2
Kristy Corkan <kcorkan@gmail.com>
2017-02-22 11:18:14 -0700

    updated planned velocity settings to planned headcount and made sure that the units are right in the headcount progress bar

--- planning-board-with-swimlanes/src/javascript/utils/overrides.js
+++ planning-board-with-swimlanes/src/javascript/utils/overrides.js
@@ -1,23 +1,98 @@
-Ext.override(Rally.ui.gridboard.planning.TimeboxColumnProgressBar,{
-    _getTotalPointCount: function() {
-        var sums = this.store.getSums();
-        var pointFieldSum = sums[this.pointField];
-        if (pointFieldSum > 0){
-            pointFieldSum = (pointFieldSum/100).toFixed(1);
-        }
 
-        return pointFieldSum || 0;
+Ext.override(Rally.ui.gridboard.planning.TimeboxColumnProgressBar,{
+
+    constructor: function(config) {
+        this.callParent(arguments);
+        this.renderTpl = Ext.create('Rally.ui.renderer.template.progressbar.TimeboxProgressBarTemplate', {
+            height: '18px',
+            width: '80%',
+            offsetWidth: '10%',
+            progressBarComponent: this,
+            /**
+             * override the text rendering functions to divide by 100 for headcount and change
+             * terminology to headcount instead of velocity
+             */
+            generateAmountCompleteText: function(recordData) {
+                return Ext.util.Format.round(recordData.amountComplete/100, 2);
+            },
+            generateChildrenTotalVelocityText: function(recordData) {
+                return recordData.childVelocity ? '(' + Ext.util.Format.round(recordData.childVelocity/100, 2) + ')' : '';
+            },
+            generateTotalPlannedVelocityText: function(recordData) {
+                // The progressbar links to "Set Planned Velocity" if there is no planned velocity for the current
+                // project and iteration (the "parent"), so we check that even though the total is what gets displayed.
+                return (recordData.parentVelocity && recordData.total) ? Ext.util.Format.round(recordData.total/100, 2) : '<a href="#">Set Planned Headcount</a>';
+            }
+        });
+        this.attribute = config.attribute;
+    },
+
+    _createTooltip: function() {
+        if (this.tooltip) {
+            this._destroyTooltip();
+        }
+        /**
+         * override for terminolgy update from Velocity to Headcount
+         * @type {Rally.ui.tooltip.ToolTip}
+         */
+        this.tooltip = Ext.create('Rally.ui.tooltip.ToolTip', {
+            cls: 'set-planned-velocity-tooltip',
+            bodyStyle: 'text-align: center;',
+            width: 150,
+            anchor: 'top',
+            target: this._getProgressBarContainer(),
+            html: 'Edit Planned Headcount'
+        });
+    },
+
+    _createPopoverWithData: function() {
+        var saveTimeboxRecord = Ext.bind(this._saveTimeboxRecord, this),
+            enableTooltip = Ext.bind(this._enableTooltip, this);
+
+        /**
+         * override to use modified popover
+         */
+        Ext.create('Rally.ui.popover.PlannedHeadcountPopover', {
+            plannedVelocity: this._getParentPlannedVelocityRollup(),
+            childVelocity: this._getChildrenPlannedVelocityRollup(),
+            hasChildren: this._hasChildren(),
+            projectName: this._getProjectName(),
+            releaseName: this._getReleaseName(),
+            target: this._getProgressBarContainer(),
+            unitName: this._getUnitName(),
+            onSaveClicked: function() {
+                var plannedVelocity = Ext.getCmp('plannedVelocityField').getValue();
+                /**
+                 * override to covert from headcount to points
+                 */
+                plannedVelocity = plannedVelocity * 100;
+                saveTimeboxRecord(plannedVelocity);
+                this.close();
+            },
+            onCancelClicked: function() {
+                enableTooltip();
+                this.close();
+            },
+            listeners: {
+                hide: function() {
+                    this._enableTooltip();
+                },
+                close: function() {
+                    this._enableTooltip();
+                },
+                scope: this
+            }
+        });
     }
 });
 
 Ext.override(Rally.ui.cardboard.row.Header, {
 
     renderTpl: null,
-    data: {},
 
     initComponent: function() {
 
-        //We need to dynamically build the render tpl since I have spent way too much time trying to tune a template
+        //We need to dynamically build the render tpl for the number of columns
 
         this.renderTpl = Ext.create('Ext.XTemplate', this._buildRenderTpl());
         this.renderData = {title: this._getTitle()};
@@ -31,24 +106,13 @@ Ext.override(Rally.ui.cardboard.row.Header, {
             if (idx === 0){
                 tpl += '<td class="row-header-cell"><div class="row-header-title">{title}</div></td>';
             } else {
-//                tpl += '<td class="row-header-cell"><div class="cell-' + idx + ' row-cell-summary" style="color:#222222;font-family:ProximaNovaSemiBold, Helvetica, Arial;border-radius:2px;font-size:11px;background-color:#e6e6e6;padding:3px;margin:2px;text-align:center;"></div></td>';
                 tpl += '<td class="row-header-cell"><div class="cell-' + idx + ' row-cell-summary"></div></td>';
             }
         }, this);
         return tpl;
     },
-    _buildRenderData: function(){
 
-        var renderData = {};
-        Ext.Array.each(this.columns, function(c, idx){
-            var key = "cell" + String.fromCharCode(97 + idx);
-            if (idx === 0){ key = "title"}
-            renderData[key] = this._getCell(c, idx);
-        }, this);
-
-        return renderData;
-    },
-    _getCell: function(col, idx){
+    _getCellText: function(col, idx){
 
         if (idx === 0){
             return this._getTitle();
@@ -69,14 +133,12 @@ Ext.override(Rally.ui.cardboard.row.Header, {
             }
         }, this);
 
-        var text = Ext.String.format("{0} of {1} Total Estimate ({2} Epics)",est, estimateTotal, epicCount);
-        return text;
-        return Ext.String.format("<span style=\"display:inline-block;color:#222222;font-family:ProximaNovaSemiBold, Helvetica, Arial;border-radius:2px;font-size:11px;background-color:#e6e6e6;padding:3px;margin:2px;text-align:center;\">{0} of {1} Total Estimate ({2} Epics)</span>",est, estimateTotal, epicCount);
+        return Ext.String.format("{0} of {1} Total Estimate ({2} Epics)",est, estimateTotal, epicCount);
     },
     onColumnsUpdated: function() {
 
         Ext.Array.each(this.columns, function(c,idx){
-            var text = this._getCell(c,idx);
+            var text = this._getCellText(c,idx);
             var cell = this.getEl().down('.cell-' + idx);
             if (cell){
                 cell.update(text);
